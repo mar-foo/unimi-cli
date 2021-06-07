d6ad413380f7fdc5dab53973c057d0a238eccfa9
Mario Forzanini <mario.forzanini@studenti.unimi.it>
2021-06-07 09:00:26 +0200

    Fix install script.

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 INSTALL_PATH="${HOME}/.local/bin"
 case "${OSTYPE}" in
-				"linux-gnu"*, *"bsd"*)	test -d ${INSTALL_PATH} || mkdir -p ${INSTALL_PATH} ; cp bin/linux/university-cli university-cli ${INSTALL_PATH} ;;
-				"darwin"*) test -d ${INSTALL_PATH} || mkdir -p ${INSTALL_PATH} ; cp bin/macos/university-cli university-cli ${INSTALL_PATH} ;;
+				"linux-"*)	test -d ${INSTALL_PATH} || mkdir -p ${INSTALL_PATH} ; cp bin/linux/university-cli university-cli.sh ${INSTALL_PATH} ;;
+				"darwin"*) test -d ${INSTALL_PATH} || mkdir -p ${INSTALL_PATH} ; cp bin/macos/university-cli university-cli.sh ${INSTALL_PATH} ;;
+				*) echo "Operating system ${OSTYPE} not supported" ;;
 esac
